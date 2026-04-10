63ea8c43915539a54e8db0e4106de4c6ad4146f7
Cyprien <cyprien.chouder@epitech.eu>
2026-04-10 11:26:46 +0200

    feat: début des premiers tests

--- Keep_White_Space/tests.js
+++ Keep_White_Space/tests.js
@@ -1,1 +1,27 @@
-import "main.js";
+require('./main.js');
+
+let failureCount = 0;
+
+function assertEqual(actual, expected) {
+    if (actual !== expected) {
+        console.error(`Assertion failed: expected "${expected}", but got "${actual}"`);
+        failureCount++;
+    } else {
+        console.log('Assertion passed');
+    }
+}
+
+const v = new Vec(1, 2);
+assertEqual(v.x, 1);
+assertEqual(v.y, 2);
+
+const t = getTimeStr(424242);
+assertEqual(t, "7:04.24");
+
+
+if (failureCount === 0) {
+    console.log('All tests passed');
+} else {
+    console.error(`Tests failed: ${failureCount}`);
+    globalThis.process.exit(1);
+}
