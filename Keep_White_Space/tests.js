7fe2d5ada8131f2e32bf3f7bac4fb2aa67bf90b5
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-04-13 12:17:42 +0200

    test: test unitaire 4

--- Keep_White_Space/tests.js
+++ Keep_White_Space/tests.js
@@ -69,6 +69,9 @@ assertEqual(v10.y, 7);
 const v11 = new Vec(0, 1).cross(new Vec(1, 0));
 assertEqual(v11, -1);
 
+const v12 = new Vec(1, 2).dot(new Vec(3, 4).add(new Vec(5, 6)));
+assertEqual(v12, 27);
+
 // ========== CALCUL LE NOMBRE D'ERREURS ===========
 if (failureCount === 0) {
     console.log('All tests passed');
